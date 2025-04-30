4e2f39da48de378426f5de2817fa7457bfdc0fbd
emmmaEnd <20031190@itcelaya.edu.mx>
2025-04-29 20:27:05 -0600

    Segundo commit

--- js/index.js
+++ js/index.js
@@ -5,4 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const view = new View();
     model.setView(view);
     view.setModel(model);
+
+    view.render();
 })
--- js/model.js
+++ js/model.js
@@ -1,16 +1,46 @@
 export default class Model {
     constructor(){
         this.view = null;
-        this.todos = [];
-        this.currentId = 1;
+        this.todos = JSON.parse(localStorage.getItem('todos'));
+        if(!this.todos || this.todos.length > 1){
+            this.todos = [
+            {
+                id:0,
+                title: 'learn JS',
+                description: 've tutos',
+                completed: false,
+            }
+            ]
+            this.currentId = 1;   
+        }else{
+            this.currentId = this.todos[this.todos.length - 1].id + 1;
+        }
+        
+        
     }
 
     setView(view){
         this.view =view;
     }
+
+    save(){
+        localStorage.setItem('todos',JSON.stringify(this.todos));
+    }
+
     setTodos(){
         return this.todos;
     }
+    findTodo(id){
+        return this.todos.findIndex((todo) => todo.id === id);
+    }
+
+    toggleCompleted(id){
+        const index = this.findTodo(id);
+        const todo = this.todos[index];
+        todo.completed = !todo.completed;
+        this.save();
+    }
+
     addTodo(title, description) {
         const todo = {
           id: this.currentId++,
@@ -22,6 +52,14 @@ export default class Model {
         this.todos.push(todo);
         console.log(this.todos);
       
+        this.save();
         return {...todo};
     }  
+
+    removeTodo(id){
+        const index = this.findTodo(id);
+        this.todos.splice(index, 1);
+        this.save();
+        //console.log(this.todos[index]);
+    }
 }
